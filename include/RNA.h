4a6af520317b3e94b2fa6962b7cbe22497027202
Megadardery <Megadardery@yahoo.com>
2018-12-05 21:00:36 +0200

    Update RNA.h

--- include/RNA.h
+++ include/RNA.h
@@ -1,16 +1,18 @@
 #ifndef _RNA_H
 #define _RNA_H
-#include "NucleicAcid.h"
+#include "Sequence.h"
 #include "Codon.h"
+#include <iosfwd>
+
 class DNA;
 class Protein;
-class RNA : public NucleicAcid
+class RNA : public Sequence
 {
 public:
 	RNA();
 	RNA(const std::string& _strand, RNAType _type = RNA_UNKNOWN);
 	RNA(const RNA& cpy);
-	RNA(const DNA& dna, bool fromMainStrand, RNAType _type = RNA_UNKNOWN);
+	RNA(const DNA& dna, bool fromMainStrand, RNAType _type = RNA_UNKNOWN, int s = -1, int e = -1);
 
 	void setType(RNAType _type);
 	RNAType getType() const;
@@ -24,13 +26,16 @@ public:
 	bool LoadSequenceFromFile(char* filename);
 	bool SaveSequenceToFile(char* filename) const;
 
+	RNA operator +(const RNA& other) const;
+	bool operator ==(const RNA& other) const;
+	bool operator !=(const RNA& other) const;
+	friend std::ostream& operator <<(std::ostream& out, const RNA& obj);
+	friend std::istream& operator >>(std::istream& in, RNA& obj);
+
 	std::string alignWith(const RNA& other) const;
 	virtual ~RNA();
 protected:
 	RNAType type;
-	std::string strand2;
-	void generate_strand1();
-	void generate_strand2();
 };
 
 #endif
